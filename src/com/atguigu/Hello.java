c43891f9a8a87a6c1db4c890b58543180acb4c49
zhangxingjin <zxjqq123@126.com>
2016-10-31 20:26:00 +0800

    Update Hello.java

--- src/com/atguigu/Hello.java
+++ src/com/atguigu/Hello.java
@@ -1,5 +1,8 @@
 package com.atguigu;
 
 public class Hello {
-
+      public static void main(String[] args) {
+	          	System.out.println("hello world!");
+          System.out.println("11111111");
+      	}
 }
